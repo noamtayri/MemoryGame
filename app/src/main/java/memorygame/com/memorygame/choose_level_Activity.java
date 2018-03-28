c077fd3e40e2e45350fa5e0bd0a113ce46e1c040
Noam <noamtayri1990@gmail.com>
2018-03-28 21:51:39 +0300

    connect choose level activity to medium level activity (MainActivity)

--- app/src/main/java/memorygame/com/memorygame/choose_level_Activity.java
+++ app/src/main/java/memorygame/com/memorygame/choose_level_Activity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class choose_level_Activity extends AppCompatActivity {
 
     public final String USER_NAME = "userName";
+
+    Spinner spinner = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +27,38 @@ public class choose_level_Activity extends AppCompatActivity {
         TextView name = (TextView)findViewById(R.id.nameTextView);
         name.setText(userName);
 
-        Integer[] levels = {4,6,8};
+        String[] levels = {"easy","medium","hard"};
 
-        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        spinner = (Spinner)findViewById(R.id.spinner);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, levels);
         spinner.setAdapter(adapter);
+
+        Button play = (Button)findViewById(R.id.playButton);
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                playBtn();
+            }
+        });
+    }
+
+    private void playBtn(){
+        Intent intent;
+        switch (spinner.getSelectedItem().toString()){
+            case "easy":
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case "medium:":
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case "hard":
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
