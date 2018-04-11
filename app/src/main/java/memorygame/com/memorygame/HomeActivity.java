57d91fd4056f588e3e1d09f6ebd647ac36e83ae8
Noam <noamtayri1990@gmail.com>
2018-04-11 15:38:36 +0300

    add name\age error msg to home activity

--- app/src/main/java/memorygame/com/memorygame/HomeActivity.java
+++ app/src/main/java/memorygame/com/memorygame/HomeActivity.java
@@ -32,6 +32,23 @@ public class HomeActivity extends AppCompatActivity {
     private void nextBtn(){
         EditText name = (EditText)findViewById(R.id.nameEditText);
         EditText age = (EditText)findViewById(R.id.ageEditText);
+        if((name.getText().toString().equals(""))){
+            name.requestFocus();
+            name.setError(getString(R.string.enter_name));
+            return;
+        }else if(age.getText().toString().equals("")){
+            age.requestFocus();
+            age.setError(getString(R.string.enter_age));
+            return;
+        }else if(Integer.parseInt(age.getText().toString()) < 1){
+            age.requestFocus();
+            age.setError(getString(R.string.positive_age));
+            return;
+        }else if(Integer.parseInt(age.getText().toString()) > 120){
+            age.requestFocus();
+            age.setError(getString(R.string.big_age));
+            return;
+        }
         Intent intent = new Intent(this, choose_level_Activity.class);
         intent.putExtra(USER_NAME, name.getText().toString());
         intent.putExtra(USER_AGE, age.getText().toString());
