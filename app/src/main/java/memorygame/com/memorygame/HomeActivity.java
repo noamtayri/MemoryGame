31ce43129362edc42f7b4d4d6568593fc5ead3cb
ronginat <ronginat51@gmail>
2018-06-01 10:13:41 +0300

    ron push

--- app/src/main/java/memorygame/com/memorygame/HomeActivity.java
+++ app/src/main/java/memorygame/com/memorygame/HomeActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
 
+    //first push
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
