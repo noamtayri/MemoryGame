481e65bc71d9d1da486d4796a9183d953b959f75
ronginat <ronginat51@gmail.com>
2018-06-07 19:51:05 +0300

    remove menu resource from project

--- app/src/main/java/memorygame/com/memorygame/RecordsActivity.java
+++ app/src/main/java/memorygame/com/memorygame/RecordsActivity.java
@@ -54,8 +54,6 @@ public class RecordsActivity extends AppCompatActivity {
 
         dbRecords = new DBHandler(this);
 
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -81,28 +79,6 @@ public class RecordsActivity extends AppCompatActivity {
         */
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_records, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
